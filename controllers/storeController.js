e1f26288bc2b297ad78fe48adad87c581c94d9cf
Dmytro Kyiashko <dmytro.kyiashko@gmail.com>
2018-05-23 20:42:22 +0300

    Add a few stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,18 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+
 exports.homePage = (req, res) => {
     res.render('index', { name: req.name });
+}
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title:'Add Store' });
+}
+
+exports.createStore = async (req,res) => {
+    const store = new Store(req.body);
+    await store.save();
+    req.flash('succes', `Successfully Created ${store.name}.` )
+    res.redirect('/');
 }
--- models/Store.js
+++ models/Store.js
@@ -3,7 +3,26 @@ mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-
+ name : {
+     type: String,
+     trim: true,
+     required: 'Please enter a store name'
+ },
+ slug: String,
+ description : {
+     type: String,
+     trim : true
+ },
+ tags: [String]
 });
 
-module.exports = mongoose.model
+storeSchema.pre('save', function(next) {
+    if(!this.isModified('name')){
+        next();
+        return;
+    }
+    this.slug = slug(this.name);
+    next();
+})
+
+module.exports = mongoose.model('Store', storeSchema);
